f1fb034ec030066fdf285281cf88957070050ba4
André <andre.o.alvarez@hotmail.com>
2020-04-16 11:10:27 -0300

    subindo 3

--- src/main/java/senac/edu/engsoft/meuproduto/service/util/LocalDateDeserializer.java
+++ src/main/java/senac/edu/engsoft/meuproduto/service/util/LocalDateDeserializer.java
@@ -1,4 +1,4 @@
-package senac.edu.engsoft.meuproduto.util;
+package senac.edu.engsoft.meuproduto.service.util;
 
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonProcessingException;
--- src/main/java/senac/edu/engsoft/meuproduto/service/util/LocalDateSerializer.java
+++ src/main/java/senac/edu/engsoft/meuproduto/service/util/LocalDateSerializer.java
@@ -1,4 +1,4 @@
-package senac.edu.engsoft.meuproduto.util;
+package senac.edu.engsoft.meuproduto.service.util;
 
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.core.JsonProcessingException;
